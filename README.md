385c70addc76242e9bdceda56714687cbdc09f9e
Olmo Buining <olmobuining@gmail.com>
2018-04-28 19:21:40 +0200

    It was not accounting for the case of the words. This modification should do the trick of being case insensitive

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ First build the image `docker build -t perl .`.
 Run the image `docker run -v <localpath>:/app -it perl`.
 Don't forget to change `<localpath>` to path of the `app` folder.
 
-
 To parse the lorem text: `perl loremParser.pl`
 You will find the result in the `lorem.result.txt` file
 
--- app/bookParser.pl
+++ app/bookParser.pl
@@ -5,19 +5,25 @@ use utf8;
 use warnings;
 
 my $ng = Text::Ngrams->new(
-		type => 'word',
-		windowsize => 2
-	);
+    type => 'word',
+    windowsize => 2
+);
 # Overrule the default token pattern, to include '
-$ng->{tokenrex} = qr/([a-zA-Z\']+|(\d+(\.\d+)?|\d*\.\d+)([eE][-+]?\d+)?)/;
+$ng->{tokenrex} = qr/([a-zA-Z\']+|(\d+(\.\d+)?|\d*\.\d+)([eE][-+]?\d+)?)/i;
 
-$ng->process_files('aliceBook.txt');
+# Convert to lowercase.
+open(my $book, '<', 'aliceBook.txt') or die "Could not read file";
+while (my $line = <$book>) {
+    $ng->process_text(lc($line));
+}
+close $book;
 
 my @ngramsarray = $ng->get_ngrams(
-	number=>2,
-	orderby=>'frequency'
+    number=>2,
+    orderby=>'frequency'
 );
 
+# Print the result to a file.
 open(my $fh, '>', 'book.result.txt') or die "Could not write to result file";
 print $fh "Count\tWords\n";
 while (my $words = shift @ngramsarray) {
